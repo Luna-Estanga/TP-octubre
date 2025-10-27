f2cfb4758e95b2d6bc2f299ce39c257019a707bc
Luna-Estanga <yasestanga@gmail.com>
2025-10-26 22:26:08 -0300

    Update pp.sql

--- pp.sql
+++ pp.sql
@@ -1,15 +1,35 @@
-CREATE TABLE alumnos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombres VARCHAR(50),
-    apellidos VARCHAR(50),
-    curso VARCHAR(50)
+CREATE DATABASE tp;
+USE tp;
+
+CREATE TABLE cursos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  anio INT,
+  division VARCHAR(10),
+  esp VARCHAR(50)
 );
 
-CREATE TABLE registros (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    tipo VARCHAR(20),
-    alumno INT,
-    materia VARCHAR(50),
-    creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (alumno) REFERENCES alumnos(id)
+CREATE TABLE materias (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100),
+  curso INT,
+  FOREIGN KEY (curso) REFERENCES cursos(id)
+);
+
+CREATE TABLE alumnos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50),
+  apellido VARCHAR(50),
+  curso INT,
+  FOREIGN KEY (curso) REFERENCES cursos(id)
+);
+
+CREATE TABLE asistencias (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  alumno INT,
+  materia INT,
+  tipo VARCHAR(5),
+  fecha DATE DEFAULT (CURRENT_DATE),
+  hora TIME DEFAULT (CURRENT_TIME),
+  FOREIGN KEY (alumno) REFERENCES alumnos(id),
+  FOREIGN KEY (materia) REFERENCES materias(id)
 );
