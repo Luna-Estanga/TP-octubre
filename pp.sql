5807dc586f7d647c004a59c7fbdc6129e29b36c4
Luna-Estanga <yasestanga@gmail.com>
2025-10-27 12:58:42 -0300

    Update pp.sql

--- pp.sql
+++ pp.sql
@@ -1,35 +1,47 @@
-CREATE DATABASE tp;
-USE tp;
+-- crear base d datos
+CREATE DATABASE asistencia;
+USE asistencia;
 
+-- Tbla e cursos
 CREATE TABLE cursos (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  anio INT,
-  division VARCHAR(10),
-  esp VARCHAR(50)
+  nombre VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE materias (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
-  curso INT,
-  FOREIGN KEY (curso) REFERENCES cursos(id)
-);
-
+-- tbla de alumnos
 CREATE TABLE alumnos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(50),
   apellido VARCHAR(50),
-  curso INT,
-  FOREIGN KEY (curso) REFERENCES cursos(id)
+  curso_id INT,
+  FOREIGN KEY (curso_id) REFERENCES cursos(id)
 );
 
+-- asistencias
 CREATE TABLE asistencias (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  alumno INT,
-  materia INT,
-  tipo VARCHAR(5),
+  alumno_id INT,
+  tipo ENUM('A','P','T','RA','AP'),
+  hora TIME,
   fecha DATE DEFAULT (CURRENT_DATE),
-  hora TIME DEFAULT (CURRENT_TIME),
-  FOREIGN KEY (alumno) REFERENCES alumnos(id),
-  FOREIGN KEY (materia) REFERENCES materias(id)
+  FOREIGN KEY (alumno_id) REFERENCES alumnos(id)
 );
+
+-- cursos
+INSERT INTO cursos (nombre) VALUES 
+('4°3 Computación'),
+('4°4 Computación');
+
+--  alumnos de 4°3 Computación
+INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
+('Yasmin', 'Luna Estanga', 1),
+('Santiago', 'Villarroel', 1),
+('Aramis', 'Perez', 1),
+('Fabricio', 'Jauregui', 1);
+
+--  alumnos de 4°4 Computación
+INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
+('Lucas', 'Brem', 2),
+('Florencia', 'Gonzales', 2),
+('Bruno', 'Zapico', 2),
+('Ian', 'Gutierrez', 2);
