c6f9977a45709fc80c583cd2b6de6ac46691c481
Luna-Estanga <yasestanga@gmail.com>
2025-10-28 19:49:08 -0300

    Add files via upload

---
+++ dd_data.sql
@@ -0,0 +1,18 @@
+-- cursos
+INSERT INTO cursos (nombre) VALUES 
+('4°3 Computación'),
+('4°4 Computación');
+
+--  alumnos de 4°3 Computación
+INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
+('Yasmin', 'Luna Estanga', 1),
+('Santiago', 'Villarroel', 1),
+('Aramis', 'Perez', 1),
+('Fabricio', 'Jauregui', 1);
+
+--  alumnos de 4°4 Computación
+INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
+('Lucas', 'Brem', 2),
+('Florencia', 'Gonzales', 2),
+('Bruno', 'Zapico', 2),
+('Ian', 'Gutierrez', 2);
