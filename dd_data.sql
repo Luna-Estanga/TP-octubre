8b3e6aa2aaea65302d658aed99c322303df52569
Luna-Estanga <yasestanga@gmail.com>
2025-10-31 13:14:30 -0300

    Update dd_data.sql

--- dd_data.sql
+++ dd_data.sql
@@ -9,10 +9,12 @@ INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
 ('Santiago', 'Villarroel', 1),
 ('Aramis', 'Perez', 1),
 ('Fabricio', 'Jauregui', 1);
+('Lucas', 'Brem', 1),
+('Florencia', 'Gonzales',1),
+('Bruno', 'Zapico', 1),
+('Ian', 'Gutierrez', 1);
 
 --  alumnos de 4°4 Computación
 INSERT INTO alumnos (nombre, apellido, curso_id) VALUES
-('Lucas', 'Brem', 2),
-('Florencia', 'Gonzales', 2),
-('Bruno', 'Zapico', 2),
-('Ian', 'Gutierrez', 2);
+
+
