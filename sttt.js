7b3aab2cb01b04c98212cdce7a45d6db43c17577
Luna-Estanga <yasestanga@gmail.com>
2025-11-04 21:44:33 -0300

    Actualizar sttt.js

--- sttt.js
+++ sttt.js
@@ -3,20 +3,20 @@ import Student from "../models/student.js";
 
 const router = express.Router();
 
-// Listar todos
+
 router.get("/", async (req, res) => {
   const students = await Student.findAll();
   res.json(students);
 });
 
-// Crear nuevo alumno
+
 router.post("/", async (req, res) => {
   const { dni, nombre, apellido } = req.body;
   const student = await Student.create({ dni, nombre, apellido });
   res.json(student);
 });
 
-// Actualizar estado (presente, tarde, retiro)
+
 router.put("/:id", async (req, res) => {
   const { estado } = req.body;
   const student = await Student.findByPk(req.params.id);
